c466549c7db0b0f494fa545df31f111ba3c13fa5
ethbhuvesh <gupta_b2000@yahoo.com>
2024-08-25 18:12:35 -0400

    JS test cases and coding

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreef) {
+  let degreec=((degreef-32)*5/9)
+  if(degreec%1!=0){
+    degreec=Math.round(degreec*10)/10;
+  }
+  return degreec;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreec) {
+  let degreef=((9*degreec/5) + 32);
+  if(degreef%1!=0){
+    degreef=Math.round(degreef*10)/10;
+  }
+  return degreef;
 };
 
 // Do not edit below this line
