b53a298318e2be2eee475b6d97ab1a352646a385
ethbhuvesh <gupta_b2000@yahoo.com>
2024-08-25 10:49:19 -0400

    Improved code by referencing the unit test file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, rep) {
+    let finalWord=word;
+    if(rep===0){
+        return "";
+    }
+    else if(rep<0){
+        return "ERROR";
+    }
+    for(let i=1; i<rep; i++){
+        finalWord+=word;
+    }
+    return finalWord;
 
 };
 
